138a7ab8298bed5611dba573cebf9479270fd4ab
Tom Clarke <tomc@designyourcode.co.uk>
2015-04-09 22:11:51 +0100

    Moved GA code to index. Re-minified JS.

--- assets/lib/vendor/youtube-api.js
+++ assets/lib/vendor/youtube-api.js
@@ -1,13 +1,3 @@
-//Google Tracking
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-// ga('create', 'UA-36458888-8', 'auto');
-ga('create', 'UA-XXXXXXXX-X', 'auto');
-ga('send', 'pageview');
-
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
@@ -30,7 +20,7 @@ $(document).ready(function(){
       videoTitle: getVideoTitle(ytKey)
     });
   });
-
+  
 });
 
 function getVideoTitle(vID) {
@@ -65,9 +55,7 @@ function createPlayer(playerInfo) {
     }
   });
 }
-
 function onPlayerStateChange(event) {
-
   var curId = $(event.target.c).attr('id'),
       curTitle,
       curEventData;
