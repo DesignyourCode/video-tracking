0ee4353d4cb781f691e7c4e8f4743918025093a0
Tom Clarke <tomc@designyourcode.co.uk>
2015-04-09 22:05:15 +0100

    Updated API to include GA tracking.

--- assets/lib/vendor/youtube-api.js
+++ assets/lib/vendor/youtube-api.js
@@ -1,3 +1,13 @@
+//Google Tracking
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+// ga('create', 'UA-36458888-8', 'auto');
+ga('create', 'UA-XXXXXXXX-X', 'auto');
+ga('send', 'pageview');
+
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
@@ -9,16 +19,33 @@ $(document).ready(function(){
   $('[id^="player-"]').each(function() {
 
     var height = $(this).data('height') ? $(this).data('height') : 390,
-        width = $(this).data('width') ? $(this).data('width') : 640;
+        width = $(this).data('width') ? $(this).data('width') : 640,
+        ytKey = $(this).attr('id').replace('player-', '');
 
     playerInfoList.push({
       id: $(this).attr('id'),
       height: height,
       width: $(this).data('width'),
-      videoId: $(this).attr('id').replace('player-', '')
+      videoId: ytKey,
+      videoTitle: getVideoTitle(ytKey)
     });
   });
+
 });
+
+function getVideoTitle(vID) {
+  var result = '';
+  
+  $.ajax({
+    url: '//gdata.youtube.com/feeds/api/videos/' + vID + '?v=2&alt=json',
+    async: false,
+    success:function(data) {
+      result = data.entry.title.$t; 
+    }
+  });
+  
+  return result;
+}
 
 function onYouTubeIframeAPIReady() {
   if(typeof playerInfoList === 'undefined') {
@@ -26,12 +53,45 @@ function onYouTubeIframeAPIReady() {
   }
   for(var i = 0; i < playerInfoList.length;i++) {
     var curplayer = createPlayer(playerInfoList[i]);
-  }   
+  }
 }
 function createPlayer(playerInfo) {
   return new YT.Player('player-' + playerInfo.videoId, {
     height: playerInfo.height,
     width: playerInfo.width,
-    videoId: playerInfo.videoId
+    videoId: playerInfo.videoId,
+    events: {
+      'onStateChange': onPlayerStateChange
+    }
   });
+}
+
+function onPlayerStateChange(event) {
+
+  var curId = $(event.target.c).attr('id'),
+      curTitle,
+      curEventData;
+
+  for(var i = 0; i < playerInfoList.length; i++) {
+    if (curId === playerInfoList[i].id) {
+      curTitle = playerInfoList[i].videoTitle;
+    }
+  }
+
+  if (event.data === -1) {
+    curEventData = 'Unstarted';
+  } else if (event.data === 0) {
+    curEventData = 'Ended';
+  } else if (event.data === 1) {
+    curEventData = 'Playing';
+  } else if (event.data === 2) {
+    curEventData = 'Paused';
+  } else if (event.data === 3) {
+    curEventData = 'Buffering';
+  } else if (event.data === 5) {
+    curEventData = 'Video cued';
+  }
+
+  console.log('send, event, Videos, ' + curTitle + ', ' + curEventData + ' (' + event.data + '), ' + parseInt(event.target.getCurrentTime()) );
+  //ga('send', 'event', 'Videos', curTitle, curEventData + ' (' + event.data + ')', parseInt(event.target.getCurrentTime()) );
 }
